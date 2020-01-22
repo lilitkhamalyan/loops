1aa4c70a1b16786c162c5cc0617c7843e6336ea5
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-01-22 15:54:44 -0500

    Added comments.

--- loops.py
+++ loops.py
@@ -1,9 +1,12 @@
 i = 0
+# Get the number from the user
 number = int(input("Enter a number from 1 to 20: "))
 
+# Number should be [1, 20]
 while not number in range (1, 21):
     number = int(input("Error! Enter a number from 1 to 20: "))
-    
+
+# i < number, print i^2
 while (i < number):
     print(i ** 2)
     i = i + 1
